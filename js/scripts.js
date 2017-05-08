02f0d785004315f30075e9ed0875c2dcfbfa4eb6
Jonathan Chen <chaochen42@gmail.com>
2017-05-08 18:40:54 +1200

    navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
 
+  $('#mobile-menu').click(function(){
+      $(this).next().slideToggle('fast');
+      $(this).find('img').toggle();
+      // $('.home-header').toggleClass('menu-open');
+  });
+
+
+  $('.carousel').bcSwipe({ threshold: 50 });
+
+});
